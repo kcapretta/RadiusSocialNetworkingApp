62de3bd1a9436c96a1869ac322c48d6c1d2f0797
Kassandra <46635962+kcapretta@users.noreply.github.com>
2019-12-18 15:33:16 -0800

    Create RadiusConceptFile.swift

---
+++ RadiusConceptFile.swift
@@ -0,0 +1,66 @@
+//
+//  InfoFile.swift
+//  Radius
+//
+//  Created by Kassandra Capretta on 11/26/19.
+//  Copyright © 2019 Kassandra Capretta. All rights reserved.
+//
+
+import Foundation
+
+//Firestore Functionality:
+//Create
+//Read
+//Update
+//Delete
+
+
+/*
+ 1. Images
+ 2. User Info
+ 3. User Signup / Login
+ */
+/*
+ ## Images
+ 1. Profile picture
+ 2. Account pictures/User images
+ 3. Swipe view
+ */
+/*
+ ## User Info
+ 1. Profile page, including questions and vitals
+ */
+/*
+ * Match someone
+ * Deny someone
+ * Chat with someone
+ * List matches
+ * List intentions (dating, networking, friendship)
+ */
+
+// Flow Structure of Radius:
+
+/*
+ 1. Signup
+ 2. Onboarding Introduction
+ 3. Gathering User Information
+ 4. Upload Profile Picture
+ 5. Allow Location Use
+ 6. Answer Questions
+ 7. Edit Profile
+ -
+ 1. Login / Forgot Passowrd
+ 2. Radius Screen (geofencing bars and coffee shops)
+ 3. Select Radius / Swipe Screen
+ 4. Match / Deny
+ 5. Messenger
+ -
+ Additonally:
+ - Edit Preferences
+ - View Likes
+ - Edit Profile
+ - Edit Questions
+ - View Your Profile
+ - Contact Help Center
+ - Reach Website
+ */
