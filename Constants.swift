f85940453962cd92074499bdbcbbc47c6fd13f76
Kasey <46635962+kcapretta@users.noreply.github.com>
2019-12-18 12:17:28 -0800

    Create Constants.swift

---
+++ Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Radius
+//
+//  Created by Kassandra Capretta on 11/5/19.
+//  Copyright © 2019 Kassandra Capretta. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+
+    struct Storyboard {
+        
+        let entryPageViewController = "HomeVC"
+    }
+}
