6b151dec68cb198f88451e8df22287be45e5747b
Kassandra <46635962+kcapretta@users.noreply.github.com>
2019-12-31 10:01:59 -0800

    Create UIImageView+Extensions.swift

---
+++ Extensions/UIImageView+Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  UIImageView+Extensions.swift
+//  Radius
+//
+//  Created by Kassandra Capretta on 12/22/19.
+//  Copyright © 2019 Kassandra Capretta. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                print("IMAGE... ", image)
+                self.image = image
+            }
+            }.resume()
+    }
+    
+    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: contentMode)
+    }
+}
