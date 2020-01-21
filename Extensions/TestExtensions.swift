6cb95f18b1a82b3b27a7b79d00fcde9a05ee38c4
Kassandra <46635962+kcapretta@users.noreply.github.com>
2020-01-21 10:37:55 -0800

    Create TestExtensions.swift

---
+++ Extensions/TestExtensions.swift
@@ -0,0 +1,44 @@
+//
+//  UITextField+Extensions.swift
+//  Radius
+//
+//  Created by Kassandra Capretta on 1/17/20.
+//  Copyright © 2020 Kassandra Capretta. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField{
+    
+    @IBInspectable var doneAccessory: Bool{
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction()
+    {
+        self.resignFirstResponder()
+    }
+}
